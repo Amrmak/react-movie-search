216a136e9f47449674d064694436c254e166c1b5
Amr <amrmak@gmail.com>
2018-03-17 00:06:24 +0200

    removing test fetch and console log in landing page

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -4,31 +4,12 @@ export default class Landing extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      query: "",
-      movieID: 284054, //Interstellar
-      movie: {}
+      query: ""
     }
-  }
-
-  componentDidMount = () => {
-    fetch(
-      `https://api.themoviedb.org/3/movie/${
-        this.state.movieID
-      }?&api_key=0324eb4b8130330ff1662244a7f60777`
-    )
-      .then(res => res.json())
-      .then(data => {
-        console.log(data)
-        this.setState({ movie: data })
-      })
-      .catch(err => {
-        console.log(err)
-      })
   }
 
   handleSearch = e => {
     e.preventDefault()
-    console.log(this.state.query)
     this.props.history.push(`/results/${this.state.query}`)
   }
 
